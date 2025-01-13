98d425197b27f42007e04d96dcca74e515d5e24e
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2025-01-13 16:32:01 +0100

    add general callbacks

---
+++ pastasdash/application/callbacks/__init__.py
@@ -0,0 +1,21 @@
+from pastasdash.application.callbacks.general import register_general_callbacks
+from pastasdash.application.callbacks.model import register_model_callbacks
+from pastasdash.application.callbacks.overview import register_overview_callbacks
+
+
+def register_callbacks(app, pstore):
+    """Register all the necessary callbacks for the application.
+
+    This function registers various callback functions to the provided app instance.
+    It organizes the registration into several categories.
+
+    Parameters
+    ----------
+    app : object
+        The application instance to which the callbacks will be registered.
+    pstore : object
+        The pastastore interface that will be used by the callbacks.
+    """
+    register_general_callbacks(app, pstore)
+    register_overview_callbacks(app, pstore)
+    register_model_callbacks(app, pstore)
