bcaf015df8626a2fee215639959ed24ffa0e1dc6
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2025-01-17 17:35:07 +0100

    rename settings

--- pastasdash/application/settings.py
+++ pastasdash/application/settings.py
@@ -3,15 +3,14 @@ from pathlib import Path
 import tomli
 
 # %% load settings
-DATALENS_APP_ROOT = Path(__file__).parent.parent
-DATALENS_APP_PATH = Path(__file__).parent
+PASTASDASH_APP_ROOT = Path(__file__).parent.parent
+PASTASDASH_APP_PATH = Path(__file__).parent
 
-with open(DATALENS_APP_PATH / "config.toml", "rb") as f:
+with open(PASTASDASH_APP_PATH / "config.toml", "rb") as f:
     config = tomli.load(f)
     settings = config["settings"]
 
 # %% set paths accordingly
 
-ASSETS_PATH = DATALENS_APP_PATH / ".." / "assets"
-LOCALE_PATH = ASSETS_PATH / "locale"
+ASSETS_PATH = PASTASDASH_APP_PATH / ".." / "assets"
 CUSTOM_CSS_PATH = str(ASSETS_PATH / "custom.css")
